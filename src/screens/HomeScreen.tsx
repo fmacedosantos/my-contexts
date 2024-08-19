63399c3b64bc25311a0a08ab837499ee6d14b25a
Felipe Macedo dos Santos <felipemacedo986@gmail.com>
2024-08-19 18:30:23 -0300

    Configurando a Home e passando as propriedades para a UserScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import { RootStackParamList } from '../../App';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-export default function HomeScreen() {
+type HomeScreenProps = {
+  navigation: StackNavigationProp<RootStackParamList, "Home">
+}
+
+export default function HomeScreen({navigation}: HomeScreenProps) {
+  const  [input, setInput] = useState("")
+
+  const navigateToUserScreen = () => {
+    navigation.navigate('User', {username: input})
+  }
+
   return (
     <View>
-        <Text>Works!</Text>
+        <TextInput placeholder='Digite seu nome:'
+        value={input} onChangeText={(text) => setInput(text)}/>
+        <Button title='Entrar' onPress={navigateToUserScreen}/>
     </View>
   );
 }
