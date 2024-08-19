69fbdf824bcccf0885a66f6896c5a7af05d4a232
Felipe Macedo dos Santos <felipemacedo986@gmail.com>
2024-08-19 18:38:08 -0300

    Acessando o valor de username em UserScreen

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,10 +1,18 @@
+import { RouteProp } from '@react-navigation/native';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { RootStackParamList } from '../../App';
 
-export default function UserScreen() {
+type UserScreenProps = {
+  route: RouteProp<RootStackParamList, "User">
+}
+
+export default function UserScreen({route}: UserScreenProps) {
+  const {username} = route.params
+
   return (
     <View>
-        <Text>Works!</Text>
+        <Text>Bem vindo(a) {username}</Text>
     </View>
   );
 }
